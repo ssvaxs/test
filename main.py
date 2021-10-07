ca972f0f4c1d72b5fd2205d72ae72b080b4a533b
Semion Vakhrushev <ssvaxs@gmail.com>
2021-10-07 14:28:17 +0300

    start

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
+class Person:
+    def __init__(self, name):
+        self.__name = name
+        self.__age = 1
 
-def print_hi(name):
-    print(f'Hi, {name}')
+    @property
+    def age(self):
+        return self.__name
+
+    @age.setter
+    def age(self, age):
+        if age in range(1, 100):
+            self.__age = age
+        else:
+            print('Incorrect age')
+
+    @property
+    def get_name(self):
+        return self.__name
+
+    def display_info(self):
+        print("Name:", self.__name, ",\tage:", self.__age)
+
+
+def main():
+    tom = Person("Tom")
+    tom.age = -10
+    tom.age = 20
+    tom.display_info()
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
